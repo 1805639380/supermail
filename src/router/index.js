ff992dc1f245f6972485782019a277b18416b484
1805639380 <1805639380@qq.com>
2021-06-12 17:14:21 +0800

    'init'

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: '',
+    redirect: '/home'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/home',
+    name: 'Home',
+    component: () => import('../views/home/Home.vue')
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: () => import('../views/cart/Cart.vue')
+  },
+  {
+    path: '/category',
+    name: 'category',
+    component: () => import('../views/category/Category.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: () => import('../views/profile/Profile.vue')
   }
 ]
 
---
+++ src/views/home/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <template v-slot:center>
+          <h3>购物街</h3>
+      </template>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from 'components/common/navbar/NavBar'
+import {getHomeMultidata} from 'network/home.js'
+
+export default {
+  components: {
+    NavBar
+  },
+  data() {
+    return {
+      banners: []
+    }
+  },
+  created() {
+    getHomeMultidata().then(res => {
+      console.log(res);
+      this.banners = res
+    })
+  }
+}
+</script>
+
+<style>
+.home-nav {
+  background-color: rgb(245, 107, 130);
+  color: #fff;
+}
+</style>
